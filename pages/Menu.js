02d007240805e57fadabf30463d7fb9a0513469e
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-06-16 16:43:04 -0400

    add menu content

--- pages/Menu.js
+++ pages/Menu.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div>
+      <div>
+        <p>La panaderia</p>
+        <p>
+          Descubre nuestra selección de panes y pastelería disponible para
+          llevar y para envío a domicilio.
+        </p>
+        <button>Pedir ahora</button>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Image src="/bollo-guella-logo.png" width={100} height={100} />
+            <h4>Pan rústico</h4>
+            <p>Harina francesa, harina japonesa, huevo, manteca de Isigny</p>
+          </li>
+          <li>
+            <Image src="/bollo-guella-logo.png" width={100} height={100} />
+            <h4>Rollo de canela</h4>
+            <p>Harina francesa, huevo, manteca de Isigny, canela</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
