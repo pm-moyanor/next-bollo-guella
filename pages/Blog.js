47d6bea016e6b38d0a700bfc51aac7ace6ac236e
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-06-19 17:34:38 -0400

    add blog content

--- pages/Blog.js
+++ pages/Blog.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div>
+      <div>
+        <p>Detrás de la cocina</p>
+        <p>
+          Lee nuestro blog para acceder a recetas y noticias y para conocer
+          mejor lo que sucede en nuestra cocina.
+        </p>
+        <button>Leer más</button>
+      </div>
+      <Image src="/bollo-guella-logo.png" width={100} height={100} />
+      <div>
+        <span>---</span>
+        <p>17/7/20</p>
+        <p>Bollo Güella fue votada como la mejor panadería local </p>
+        <span>---</span>
+        <p>17/7/20</p>
+        <p>Nuestra receta de croissant clásico </p>
+        <span>---</span>
+        <p>17/7/20</p>
+        <p>Cómo empezar a hacer pan </p>
+        <span>---</span>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
--- pages/Hero.js
+++ pages/Hero.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <Image src="/bollo-guella-logo.png" width={100} height={100} />
+      <h2>
+        Creemos que la calidad de nuestro pan depende de los ingredientes que
+        usemos
+      </h2>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
