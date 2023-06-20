eb36c9ab14ad0ef5e803a9729f257f9bcc1fb14b
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-06-20 17:40:02 -0400

    add contact content

--- pages/Contact.js
+++ pages/Contact.js
@@ -3,8 +3,18 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <p>123 Calle Demo</p>
-      <p>Maipu, Mendoza.</p>
+       <h3> Bollo Güella</h3>
+      <div>
+        <p>123 Calle Demo</p>
+        <p>Maipu, Mendoza.</p>
+      </div>
+      <div>
+        <p>Regístrate con tu dirección de correo electrónico para recibir noticias y actualizaciones.</p>
+        <form>
+            <input type="text" placeholder="Correo electrónico"></input>
+            <button>Registrarse</button>
+        </form>
+      </div>
     </div>
   );
 };
