97bf890e88aea6ef7c4f65da7a7fc14c901c871c
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-11-09 17:50:17 -0500

    fix blog margin

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,32 @@
-import React from "react";
-import styles from "../styles/Header.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Header.module.css";
 import Image from "next/image";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const shouldHideHeader = scrollPosition > 100;
+      setIsScrolled(shouldHideHeader);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-        
-        <nav className={`${styles.headerWrapper} ${styles.fixedHeader}`}>
-        <Image src="/bollo-guella-logo.png" width={100} height={100}/>
+      <nav
+        className={`${styles.headerWrapper} ${
+          isScrolled ? styles.fixedHeader : ""
+        } ${isScrolled ? styles.hiddenHeader : ""}`}
+      >
+        <Image src="/bollo-guella-logo.png" width={100} height={100} />
         <ul className={styles.navList}>
           <li>
             <a href="#about">Acerca de</a>
