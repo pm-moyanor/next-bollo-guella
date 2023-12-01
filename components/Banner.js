05601eec825d148a2e19a0996a121c679b234ac5
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-12-01 11:45:32 -0500

    responsive banner

--- components/Banner.js
+++ components/Banner.js
@@ -8,44 +8,47 @@ const Banner = () => {
   return (
     <div className={styles.container}>
       <motion.div
-        initial={{ x: -200, opacity: 0 }} 
-        animate={{ x: 0, opacity: 1 }} 
-        transition={{ duration: 1 }} 
+        className={styles.largeImgWrapper}
+        initial={{ x: -200, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
       >
         <Image
           src="/gaelle-marcel-wPOV0W7ykxE-unsplash.jpg"
-          width={630}
-          height={900}
-          
+          layout="fill" 
+          objectFit="cover"
+   
         />
-       </motion.div>
+      </motion.div>
 
       <div className={styles.wrapper}>
-      <motion.div
-        initial={{ y: -200, opacity: 0 }} 
-        animate={{ y: 0, opacity: 1 }} 
-        transition={{ duration: 1 }} 
-      >
-        <h2 style={{ fontFamily: "Montserrat, sans-serif" }}>
-          Nuestra filosofía para hacer pan se basa en el equilibrio
-        </h2>
+        <motion.div
+          initial={{ y: -200, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <h2 style={{ fontFamily: "Montserrat, sans-serif" }}>
+            Nuestra filosofía para hacer pan se basa en el equilibrio
+          </h2>
         </motion.div>
         <div>
           <div className={styles.aboutWrapper}>
             <motion.div
-              initial={{ x:200, opacity: 0 }}
-              animate={{x:0, opacity: 1 }}
-              transition={{ duration: 1}}
+              className={styles.image}
+              initial={{ x: 200, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
             >
               <Image
                 src="/jennifer-burk-wV1Q_vvc1GY-unsplash.jpg"
-                width={240}
-                height={350}
+               height={200}
+               width={100}
+                layout="responsive"
               />
             </motion.div>
             <div className={styles.aboutTextWrapper}>
-              <p style={{ paddingBottom: 30 }}>De la granja a la mesa</p>
-              <p style={{ width: 300, fontWeight: "200" }}>
+              <p style={{ paddingBottom: 20 }}>De la granja a la mesa</p>
+              <p >
                 Bollo Güella es una panadería de Mendoza que elabora panes y
                 pastelería artesanal hecha a mano con técnicas tradicionales.
               </p>
