3574cc251a132efb845656e7c5094990977553e0
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-12-01 10:03:28 -0500

    mobile menu

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Header.module.css";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
+
+  const handleToggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleResize = () => {
+    const isLarge = window.innerWidth > 768;
+    setIsLargeScreen(isLarge);
+    if (isLarge) {
+      setIsMobileMenuOpen(false);
+    }
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const shouldHideHeader = scrollPosition > 100;
-      setIsScrolled(shouldHideHeader);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div>
-      <nav
-        className={`${styles.headerWrapper} ${
-          isScrolled ? styles.fixedHeader : ""
-        } ${isScrolled ? styles.hiddenHeader : ""}`}
-      >
-        <Image src="/bollo-guella-logo.png" width={100} height={100} />
-        <ul className={styles.navList}>
-          <li>
-            <a href="#about">Acerca de</a>
-          </li>
-          <li>
-            <a href="#menu">Menu</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="#contact">Contacto</a>
-          </li>
-        </ul>
+      <nav className={`${styles.headerWrapper}`}>
+        <Image src="/bollo-guella-logo.png" width={80} height={80} />
+        {isLargeScreen ? (
+          <ul className={`${styles.navList}`}>
+            <li>
+              <a href="#about">Acerca de</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="#contact">Contacto</a>
+            </li>
+          </ul>
+        ) : (
+          <div className={styles.mobileMenu}>
+            <button
+              onClick={handleToggleMenu}
+              className={styles.mobileMenuButton}
+            >
+              {isMobileMenuOpen ? (
+                <FontAwesomeIcon icon={faTimes} size="sm" />
+              ) : (
+                <FontAwesomeIcon icon={faBars} size="sm" />
+              )}
+            </button>
+            {isMobileMenuOpen && (
+              <ul className={`${styles.navList} ${styles.mobileMenu}`}>
+                <li>
+                  <a href="#about">Acerca de</a>
+                </li>
+                <li>
+                  <a href="#menu">Menu</a>
+                </li>
+                <li>
+                  <a href="/blog">Blog</a>
+                </li>
+                <li>
+                  <a href="#contact">Contacto</a>
+                </li>
+              </ul>
+            )}
+          </div>
+        )}
       </nav>
     </div>
   );
